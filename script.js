b64d47cbe3b16104fb3f26792a2975b034f33d26
tizhproger <35668044+tizhproger@users.noreply.github.com>
2023-08-07 00:40:10 +0200

    Fixes
    Fixed cross playing error, then different tabs were playing sounds
    Now only active tab will send data

--- script.js
+++ script.js
@@ -69,7 +69,7 @@
         transfer_interval = setInterval(()=>{
             let hostname = window.location.hostname;
             // TODO: maybe add more?
-            if (hostname === 'soundcloud.com') {
+            if (hostname === 'soundcloud.com' && !document.hidden) {
                 let status = query('.playControl', e => e.classList.contains('playing') ? "playing" : "stopped", 'unknown');
                 let cover = query('.playbackSoundBadge span.sc-artwork', e => e.style.backgroundImage.slice(5, -2).replace('t50x50','t500x500'));
                 let title = query('.playbackSoundBadge__titleLink', e => e.title);
@@ -92,7 +92,7 @@
                     conn.send(JSON.stringify({cover, title, artists, status, progress, duration, album_url, album}));
                 }
 
-            } else if (hostname === 'open.spotify.com') {
+            } else if (hostname === 'open.spotify.com' && !document.hidden) {
                 let data = navigator.mediaSession;
                 let album = data.metadata.album;
                 let status = query('.vnCew8qzJq3cVGlYFXRI', e => e === null ? 'stopped' : (e.getAttribute('aria-label') === 'Play' || e.getAttribute('aria-label') === 'Слушать' ? 'stopped' : 'playing'));
@@ -106,7 +106,7 @@
                 if (title !== null && (status == "playing" || status == "playing")) {
                     conn.send(JSON.stringify({ cover, title, artists, status, progress, duration, album }));
                 }
-            } else if (hostname === 'www.youtube.com') {
+            } else if (hostname === 'www.youtube.com' && !document.hidden) {
                 if (!navigator.mediaSession.metadata) // if nothing is playing we don't submit anything, otherwise having two youtube tabs open causes issues
                     return;
                 let artists = [];
@@ -145,7 +145,7 @@
                         conn.send(JSON.stringify({ cover, title, artists, status, progress: Math.floor(progress), duration }));
                     }
                 }
-            } else if (hostname === 'music.youtube.com') {
+            } else if (hostname === 'music.youtube.com' && !document.hidden) {
                 if (!navigator.mediaSession.metadata) // if nothing is playing we don't submit anything, otherwise having two youtube tabs open causes issues
                     return;
                 // Youtube Music support by Rubecks
