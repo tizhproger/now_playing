2ee247092887bf28299e892cd6f97ee9677e9adb
tizhproger <35668044+tizhproger@users.noreply.github.com>
2023-08-16 03:50:35 +0200

    Console messages + closing handling

--- Python server/server.py
+++ Python server/server.py
@@ -11,17 +11,26 @@ obs_reciever = set()
 
 # The main behavior function for this server
 async def echo(websocket):
-    global obs_reciever
-    print("A browser client connected")
+    global obs_reciever, connected
     # Store a copy of the connected client
-    if websocket not in connected:
-        connected.add(websocket)
+    
     # Handle incoming messages
     try:
         async for message in websocket:
             if message == 'obs-source':
                 obs_reciever.add(websocket)
                 print("Source in OBS connected")
+                
+            elif 'connected - ' in message:
+                print(message.split('-')[1] + " tab was connected")
+                if websocket not in connected:
+                    connected.add(websocket)
+                
+            elif 'closed - ' in message:
+                print(message.split('-')[1] + " tab was closed/reloaded")
+                if websocket in connected:
+                    connected.remove(websocket)
+                
             else:
                 if len(obs_reciever) > 0:
                     for obs in obs_reciever:
@@ -32,7 +41,8 @@ async def echo(websocket):
     except websockets.exceptions.ConnectionClosed as e:
         print("A client just disconnected")
     finally:
-        connected.remove(websocket)
+        if websocket in connected:
+            connected.remove(websocket)
 
 # Start the server
 start_server = websockets.serve(echo, "localhost", PORT)
