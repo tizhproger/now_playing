cc3b77b62b712209b2ec98e22989bd6a177dc0c6
tizhproger <35668044+tizhproger@users.noreply.github.com>
2023-09-05 03:27:29 +0200

    Updating code

--- Server GUI/server.py
+++ Server GUI/server.py
@@ -1,10 +1,9 @@
 import websockets
-import sys
 import asyncio
 
 clients = set()
 recievers = set()
-gui_reciever = ''
+exit = ''
 
 # The main behavior function for this server
 async def echo(websocket):
@@ -12,30 +11,21 @@ async def echo(websocket):
     # Handle incoming messages
     try:
         async for message in websocket:
-            if message == 'obs-source':
+            if message == 'obs-source' and websocket not in recievers:
                 recievers.add(websocket)
-                await gui_message("Source in OBS connected")
                 print("Source in OBS connected")
 
-            elif 'twitch' in message:
+            elif 'twitch' in message and websocket not in recievers:
                 recievers.add(websocket)
-                await gui_message("Twitch bot connected")
                 print("Twitch bot connected")
-            
-            elif 'gui' in message:
-                gui_reciever = websocket
-                await gui_message("GUI connected")
-                print("GUI connected")
                 
-            elif 'connected - ' in message:
+            elif 'connected - ' in message and websocket not in clients:
                 print(message.split('-')[1] + " was connected")
-                await gui_message(message.split('-')[1] + " was connected")
                 if websocket not in clients:
                     clients.add(websocket)
                 
-            elif 'closed - ' in message:
+            elif 'closed - ' in message and websocket not in clients:
                 print(message.split('-')[1] + " was closed/reloaded")
-                await gui_message(message.split('-')[1] + " was closed/reloaded")
                 if websocket in clients:
                     clients.remove(websocket)
                 
@@ -51,25 +41,30 @@ async def echo(websocket):
     finally:
         if websocket in clients:
             clients.remove(websocket)
-    
-
-async def gui_message(message):
-    await gui_reciever.send(message)
 
 
-def run(ipaddress, port):
+async def echo_server(ipaddress, port, exit_):
+    async with websockets.serve(echo, ipaddress, port):
+        await exit_
+
+
+def run_webserver(ipaddress, port):
+    global exit
+
+    asyncio.set_event_loop(asyncio.new_event_loop())
     loop = asyncio.get_event_loop()
 
     try:
         print("Server listening on Port " + str(port))
-        start_server = websockets.serve(echo, ipaddress, port)
-        loop.run_until_complete(start_server)
+        exit = asyncio.Future()
+        loop.create_task(echo_server(ipaddress, port, exit))
         loop.run_forever()
+        loop.close()
     except Exception as e:
             print('Server error - ' + str(e))
-    
 
-if __name__ == "__main__":
-    ip_address = sys.argv[1]
-    port = sys.argv[2]
-    run(ip_address, port)
+
+def close_webserver():
+    global exit
+
+    exit.set_result('exit')
